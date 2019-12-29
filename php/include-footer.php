c83087624574c53f62775fcfd4c1969dc4204b9f
code <33802433+codeacade@users.noreply.github.com>
2019-12-30 00:50:26 +0100

    testing php include

---
+++ php/include-footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div>
+  <a href="#" title="random1">Random 1</a>
+  <a href="#" title="random2">Random 2</a>
+  <a href="#" title="random3">Random 3</a>
+  </div>
+</footer>
+</body>
+</html>
