2e75939d20f3b86dedf5d58ee83cb6d0830b189f
code <33802433+codeacade@users.noreply.github.com>
2019-12-30 00:51:29 +0100

    testing php include

---
+++ php/include-head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>include exercise</title>
+  <link rel="stylesheet" type="text/css" href="css.css" />
+  <style></style>
+  <script></script>
+</head>
+<body>
