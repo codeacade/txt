7ce800e98d1120bd03440ae52c98e5c562bd032e
code <33802433+codeacade@users.noreply.github.com>
2019-12-30 00:49:54 +0100

    testing php include

---
+++ php/include.php
@@ -0,0 +1,11 @@
+<?php include 'include-head.php' ?>
+<header>
+<img src="php.png" width="100" alt="php" />
+<div>
+<a href="#" title="HOME">HOME</a>
+<a href="#" title="ABOUT US">ABOUT US</a>
+<a href="#" title="CONTACT">CONTANCT</a>
+</div>
+</header>
+<main></main>
+<?php include 'include-footer.php' ?>
