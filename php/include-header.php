97f53412775b161fb659ec145f3fd0174a8057be
code <33802433+codeacade@users.noreply.github.com>
2020-01-03 00:34:50 +0100

    added

---
+++ php/include-header.php
@@ -0,0 +1,8 @@
+<header>
+<img src="php.png" width="100" alt="php" />
+<div>
+<a href="include.php" title="HOME">HOME</a>
+<a href="#" title="ABOUT US">ABOUT US</a>
+<a href="contact.php" title="CONTACT">CONTANCT</a>
+</div>
+</header>
