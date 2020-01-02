40b11716cd875f953f92447e8e75241295d8d769
code <33802433+codeacade@users.noreply.github.com>
2020-01-03 00:37:35 +0100

    added

---
+++ php/contact.php
@@ -0,0 +1,16 @@
+<?php include "include-head.php" ?>
+<?php include "include-header.php" ?>
+<main>
+  <h3>CONTACT</h3>
+  <form>
+    <label>Name: </label>
+	<input type="text" name="from-include" /><br />
+	<label>Message: </label>
+	<textarea name="message-include" >Enter text here....</textarea>
+	<br />
+	<input type="submit" name="submit" value="Submit" />
+ 
+  </form>
+
+</main>
+<?php include "include-footer.php" ?>
