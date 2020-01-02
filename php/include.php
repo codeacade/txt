bfd65cefe113253a28076ac9538104a91bcf864b
code <33802433+codeacade@users.noreply.github.com>
2020-01-03 00:36:38 +0100

    content added

--- php/include.php
+++ php/include.php
@@ -1,11 +1,11 @@
-<?php include 'include-head.php' ?>
-<header>
-<img src="php.png" width="100" alt="php" />
-<div>
-<a href="#" title="HOME">HOME</a>
-<a href="#" title="ABOUT US">ABOUT US</a>
-<a href="#" title="CONTACT">CONTANCT</a>
-</div>
-</header>
-<main></main>
-<?php include 'include-footer.php' ?>
+<?php include "include-head.php" ?>
+<?php include "include-header.php" ?>
+<main>
+  <h3>PASSWORD</h3>
+  <p>To reset your password, submit your username or 
+  your email address below. If we can find you in the database, 
+  an email will be sent to your email address, 
+  with instructions how to get access again.</p>
+
+</main>
+<?php include "include-footer.php" ?>
